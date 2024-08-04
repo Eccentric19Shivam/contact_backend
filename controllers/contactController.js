d1544b0746b15a3f7c886bdf64e13977c7e43ef4
Eccentric19Shivam <singhshivamhp16@gmail.com>
2024-08-04 19:16:45 +0530

    first commit

---
+++ controllers/contactController.js
@@ -0,0 +1,52 @@
+// @desc Get all contacts
+// @route GET /api/contacts
+// @access public
+const getContacts = (req, res) => {
+    res.status(200).json({message: "Get all contacts"});
+};
+
+// @desc Create new contact
+// @route POST /api/contacts
+// @access public
+const createContact = (req, res) => {
+    console.log("The request body is:", req.body);
+    const { name, email, phone } = req.body;
+    if(!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    res.status(201).json({message: "Create contact"});
+};
+
+// @desc get contact
+// @route GET /api/contacts/:id
+// @access public
+const getContact = (req, res) => {
+    res.status(200).json({message: `Get a contact ${req.params.id}`});
+};
+
+// @desc Update contact
+// @route PUT /api/contacts/:id
+// @access public
+const updateContact = (req, res) => {
+    res.status(200).json({message: `Update a contact ${req.params.id}`});
+};
+
+// @desc Delete contact
+// @route DELETE /api/contacts/:id
+// @access public
+const deleteContact = (req, res) => {
+    res.status(200).json({message: `Delete a contact ${req.params.id}`});
+};
+
+
+
+
+module.exports = { 
+    getContacts, 
+    createContact, 
+    getContact, 
+    updateContact, 
+    deleteContact,
+};
+
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const dotenv = require('dotenv').config();
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
+app.use(errorHandler);
+
+app. listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
