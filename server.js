c04671a81e692d896eed4d2267ce95aefe510fac
Eccentric19Shivam <singhshivamhp16@gmail.com>
2024-08-05 22:20:46 +0530

    implemented user routes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv').config();
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require('./config/dbConnection');
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -9,6 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
